871981ea4f945ccf5db7eaeb0d24797a68da8ac9
Cenaras <mathias_rud@hotmail.com>
2021-08-16 01:43:17 +0200

    Cargo and dependencies

--- rust_chat/src/main.rs
+++ rust_chat/src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
+fn get_number()
+{
+    let mut message = String::new();
+    io::stdin().read_line(&mut message).expect("Failed to read line");
+}
+
 fn main() {
+
     println!("Hello, world!");
+    get_number();
 }
