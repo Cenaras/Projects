207b6a60837db3b8b294c2cb4b156569faac8b47
Cenaras <mathias_rud@hotmail.com>
2021-08-16 02:42:08 +0200

    First basic string reads

--- rust_chat/src/main.rs
+++ rust_chat/src/main.rs
@@ -1,13 +1,24 @@
 use std::io;
+use num_bigint::BigInt;
 
-fn get_number()
+type key = (BigInt , BigInt );
+
+
+fn enc_msg(msg: &str) -> &str
+{
+    return msg;
+}
+
+fn get_msg() -> String
 {
     let mut message = String::new();
     io::stdin().read_line(&mut message).expect("Failed to read line");
+    return message;
 }
 
 fn main() {
 
-    println!("Hello, world!");
-    get_number();
+    println!("Enter a message:");
+    let result = get_msg();
+    println!("Result was: {}", result);
 }
