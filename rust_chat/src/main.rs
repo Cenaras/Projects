5cb01e7f483c68f962c32d56ca7b8f5ed97388aa
Cenaras <mathias_rud@hotmail.com>
2021-08-16 17:37:34 +0200

    read from file created, and some arithmetic tests

--- rust_chat/src/main.rs
+++ rust_chat/src/main.rs
@@ -1,12 +1,26 @@
 use std::io;
 use num_bigint::BigInt;
+//use std::env;
+use std::fs;
 
-type key = (BigInt , BigInt );
+type key = (BigInt , BigInt);
 
 
-fn enc_msg(msg: &str) -> &str
+fn enc_msg(msg: &str, n: u64, e: u64) -> &str
 {
     return msg;
+}
+
+// c = m^e mod n
+fn int_enc_msg(m: u32, n: u32, e: u32) -> u32
+{
+    return m.pow(e) % n;
+}
+
+// m = c^d mod n
+fn int_dec_msg(c: u32, n: u32, d: u32) -> u32
+{
+    return c.pow(d) % n;
 }
 
 fn get_msg() -> String
@@ -16,9 +30,22 @@ fn get_msg() -> String
     return message;
 }
 
+fn readKey(file: &str)
+{
+    
+    let contents = fs::read_to_string(file)
+        .expect("Something went wrong reading the file");
+    println!("In file was: {}", contents);
+}
+
 fn main() {
 
     println!("Enter a message:");
     let result = get_msg();
     println!("Result was: {}", result);
+    let base: i32 = 2;
+    let pow = base.pow(2);
+    println!("{}", pow%2);
+    //println!("Enc and dec: {} : {}", )
+    readKey("test.txt");
 }
