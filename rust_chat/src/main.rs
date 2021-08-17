0f717622dfbedb3277ddc5bf6a8695ba19f24fde
Cenaras <mathias_rud@hotmail.com>
2021-08-17 22:53:43 +0200

    initial work on the rsa project

--- rust_chat/src/main.rs
+++ rust_chat/src/main.rs
@@ -1,26 +1,34 @@
 use std::io;
-use num_bigint::BigInt;
-//use std::env;
+use num_bigint::BigUint;
 use std::fs;
+// Notes: https://github.com/rsarky/og-rsa/blob/master/src/lib.rs
 
-type key = (BigInt , BigInt);
+//(e, d, n)
+type KeySet = (BigUint, BigUint, BigUint);
 
 
-fn enc_msg(msg: &str, n: u64, e: u64) -> &str
+//Hard coded toy example for keypair gen
+    /*
+        n = p*q
+        d = e^-1 mod (p-1 * q-1)
+    */  
+fn createKeyPair() -> KeySet
 {
-    return msg;
+    let p = BigUint::from(5u64);
+    let q = BigUint::from(11u64);
+    return(BigUint::from(7u64), BigUint::from(23u64), p*q);
 }
 
 // c = m^e mod n
-fn int_enc_msg(m: u32, n: u32, e: u32) -> u32
+fn int_enc_msg(m: BigUint, n: BigUint, e: BigUint) -> BigUint
 {
-    return m.pow(e) % n;
+    return m.modpow(&e, &n);
 }
 
 // m = c^d mod n
-fn int_dec_msg(c: u32, n: u32, d: u32) -> u32
+fn int_dec_msg(c: BigUint, n: BigUint, d: BigUint) -> BigUint
 {
-    return c.pow(d) % n;
+    return c.modpow(&d, &n);
 }
 
 fn get_msg() -> String
@@ -30,22 +38,30 @@ fn get_msg() -> String
     return message;
 }
 
-fn readKey(file: &str)
-{
-    
-    let contents = fs::read_to_string(file)
+fn read_key(file: &str) -> String
+{ 
+    return fs::read_to_string(file)
         .expect("Something went wrong reading the file");
-    println!("In file was: {}", contents);
 }
+
+
+
 
 fn main() {
 
-    println!("Enter a message:");
-    let result = get_msg();
-    println!("Result was: {}", result);
-    let base: i32 = 2;
-    let pow = base.pow(2);
-    println!("{}", pow%2);
-    //println!("Enc and dec: {} : {}", )
-    readKey("test.txt");
+    //println!("Enter a message:");
+    //let result = get_msg();
+    //println!("Result was: {}", result);
+    
+    let file = read_key("test.txt");
+
+    let message = BigUint::from(42u64); //Hard coded message - encrypts to "48"
+    //let cipher = int_enc_msg(message, n, e);
+
+    let key: KeySet = createKeyPair();
+
+    //println!("Cipher: {}", cipher);
+    println!("Hard coded result: {}", int_dec_msg(BigUint::from(48u64), key.2, key.1));
+
+ 
 }
